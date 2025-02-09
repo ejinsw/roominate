55a48ba23c9cc694af3247d9c5667f1a3d1208aa
kennyluuuuuuu <kennyluu130@gmail.com>
2025-02-09 15:28:17 -0800

    Added folders to app in front-end

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,7 +2,7 @@ import { Clicker } from "@/components/global/Clicker";
 import { Title } from "@/components/home/Title";
 
 export default function Home() {
-  // TODO: Finish Homepage 
+  // TODO: Finish Homepage
   return (
     <div className="flex flex-col gap-8 justify-center items-center h-screen">
       {/* Example React Components */}
