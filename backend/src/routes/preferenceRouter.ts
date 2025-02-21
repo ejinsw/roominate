1ddf963d922b57fd0f711636ee5cf51bf7995e9a
Elijah Won <ejinsw@gmail.com>
2025-02-20 20:50:33 -0800

    Update preferenceRouter.ts

--- backend/src/routes/preferenceRouter.ts
+++ backend/src/routes/preferenceRouter.ts
@@ -1,13 +1,17 @@
 import { Router } from "express";
-import { createGroupPreference, createUserPreference, getAllPreferences, getGroupsByPreferenceById, getUsersByPreferenceById, updateGroupPreference, updateUserPreference } from "../controllers/preferenceController";
+import { createGroupPreference, createUserPreference, deleteGroupPreference, deleteUserPreference, getAllPreferences, getGroupsByPreferenceById, getUsersByPreferenceById, updateGroupPreference, updateUserPreference } from "../controllers/preferenceController";
 const router = Router();
 
 router.get("/preferences", getAllPreferences);
+
 router.get("/preferences/users/:id", getUsersByPreferenceById);
 router.post("/preferences/users/:id", createUserPreference);
 router.put("/preferences/users/:id", updateUserPreference);
+router.delete("/preferences/users/:id", deleteUserPreference);
+
 router.get("/preferences/groups/:id", getGroupsByPreferenceById);
 router.post("/preferences/groups/:id", createGroupPreference);
 router.put("/preferences/groups/:id", updateGroupPreference);
+router.delete("/preferences/groups/:id", deleteGroupPreference);
 
 export default router;
