d30b82a0cb09c435f2d9c5cb801ff6ce4311132c
Elijah Won <ejinsw@gmail.com>
2025-02-20 17:05:06 -0800

    Update housingRouter.ts

--- backend/src/routes/housingRouter.ts
+++ backend/src/routes/housingRouter.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
-import { } from "../controllers/userController";
+import { getAllHousing, getGroupsByHousingById, getUsersByHousingById } from "../controllers/housingController";
 const router = Router();
 
-router.post("/users", );
+router.get("/preferences", getAllHousing);
+router.get("/preferences/users/:id", getUsersByHousingById);
+router.get("/preferences/groups/:id", getGroupsByHousingById);
 
 export default router;
