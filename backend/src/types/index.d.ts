cb7222a09b8902d43255a8c288bd43f334883348
Elijah Won <ejinsw@gmail.com>
2025-03-05 14:53:23 -0800

    Update index.d.ts

--- backend/src/types/index.d.ts
+++ backend/src/types/index.d.ts
@@ -1,8 +1,26 @@
-import { User as PrismaUser } from "@prisma/client";
+import { Prisma, User as PrismaUser } from "@prisma/client";
 
 declare global {
   declare namespace Express {
-    export interface User extends PrismaUser {}
+    export interface User
+      extends Prisma.UserGetPayload<{
+        include: {
+          preferences: {
+            include: {
+              preferences: {
+                include: {
+                  preference: true
+                }
+              },
+              preferredHousing: {
+                include: {
+                  housing: true
+                }
+              }
+            };
+          };
+        };
+      }> {}
     // TODO: Add the rest of the schema (...or import types from ORM)
     export interface Request {
       user: User;
