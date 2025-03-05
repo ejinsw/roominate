7c426737c8fc108a7b99c4a2c4c6c03ce5264641
Elijah Won <ejinsw@gmail.com>
2025-03-05 14:53:42 -0800

    Add query filters to users route

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,23 +1,131 @@
 import expressAsyncHandler from "express-async-handler";
 import { NextFunction, Request, Response } from "express";
-import { Prisma } from '@prisma/client';
+import { Prisma } from "@prisma/client";
 import prisma from "../prismaClient";
 
 export const getUsers = expressAsyncHandler(
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
+      /* Parse Query Filters */
+      const { filters: queryFilters } = req.query;
+
+      // this'll contain the parsed query filters
+      let parsedFilters: {
+        preferences: string[];
+        gender: string[];
+        year: string[];
+        housing: string[];
+        major: string[];
+      } = { preferences: [], gender: [], year: [], housing: [], major: [] };
+
+      // parse out the query filters
+      if (queryFilters) {
+        try {
+          parsedFilters = JSON.parse(queryFilters as string);
+        } catch (error) {
+          res.status(400).json({ error: "Invalid filters format" });
+          return;
+        }
+      }
+
+      /* Add Filters */
       const filter = {} as Prisma.UserWhereInput;
 
+      // Name filter
       if (req.query.name) {
-        filter.name = { contains: req.query.name.toString(), mode: 'insensitive' };
+        filter.name = {
+          contains: req.query.name.toString(),
+          mode: "insensitive",
+        };
       }
+
+      // Email filter
       if (req.query.email) {
-        filter.email = { contains: req.query.email.toString(), mode: 'insensitive' };
+        filter.email = {
+          contains: req.query.email.toString(),
+          mode: "insensitive",
+        };
       }
+
+      // Id filter
       if (req.query.id) {
-        filter.id = { contains: req.query.id.toString(), mode: 'insensitive' };
+        filter.id = { contains: req.query.id.toString(), mode: "insensitive" };
       }
-  
+
+      // Gender filter
+      if (parsedFilters.gender.length) {
+        filter.gender = {
+          in: parsedFilters.gender,
+          mode: "insensitive",
+        };
+      }
+
+      // Year filter
+      if (parsedFilters.year.length) {
+        filter.year = { in: parsedFilters.year.map((val) => parseInt(val)) };
+      }
+
+      // Major filter
+      if (parsedFilters.major.length) {
+        filter.major = {
+          in: parsedFilters.major,
+          mode: "insensitive",
+        };
+      }
+
+      // Preference filters
+      if (parsedFilters.preferences.length && filter.preferences && req.user) {
+        filter.preferences = {
+          preferences: {
+            every: {
+              AND: parsedFilters.preferences.map((preference) => {
+                // this finds the matching user pref
+                const userPreference = req.user?.preferences?.preferences
+                  ? req.user.preferences.preferences.find(
+                      (val) => val.preference.value === preference
+                    )
+                  : null;
+
+                if (!userPreference) return {}; // otherwise skips
+
+                // filters out the users that don't have the same selected option for the preference
+                return {
+                  preference: {
+                    value: {
+                      equals: preference,
+                      mode: "insensitive",
+                    },
+                  },
+                  option: {
+                    equals: userPreference.option,
+                  },
+                };
+              }),
+            },
+          },
+        };
+      }
+
+      // Housing filter
+      if (parsedFilters.housing.length && filter.preferences && req.user) {
+        filter.preferences = {
+          preferredHousing: {
+            some: {
+              housing: {
+                OR: parsedFilters.housing.map((housing) => {
+                  return {
+                    name: {
+                      equals: housing,
+                      mode: "insensitive",
+                    },
+                  };
+                }),
+              },
+            },
+          },
+        };
+      }
+
       const users = await prisma.user.findMany({ where: filter });
       res.json(users);
     } catch (error) {
