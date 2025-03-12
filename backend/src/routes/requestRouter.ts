6d4974049ff258326b2e84b6a09056f5068685a2
Elijah Won <ejinsw@gmail.com>
2025-03-12 16:47:34 -0700

    Fix request router

--- backend/src/routes/requestRouter.ts
+++ backend/src/routes/requestRouter.ts
@@ -1,7 +1,17 @@
-// association goes from user to group.
+// association goes from group to user.
+// endpoints send invite, accept invite, reject invite
 import { Router } from "express";
-import { getRequests } from "../controllers/requestController";
+import {
+  acceptRequest,
+  rejectRequest,
+  sendRequest,
+  getRequests,
+} from "../controllers/requestController";
 const router = Router();
 
+router.post("/requests/send", sendRequest);
+router.put("/requests/:id/reject", rejectRequest);
+router.put("/requests/:id/accept", acceptRequest);
 router.get("/requests/:id", getRequests);
+
 export default router;
